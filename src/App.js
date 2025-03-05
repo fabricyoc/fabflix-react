6d6620964b42ca6403ed76ffbeb6dcd45e8bdab2
fabricyo <fazin_c@hotmail.com>
2025-03-05 11:07:52 -0300

    Refatoração de Categoria com a constante e sua função dentro do index

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
 import Banner from "./componentes/Banner";
 import Card from "./componentes/Card";
-import Category from "./componentes/Category";
+// importa o padrão Category e entre {} importação nomeada de funções e variáveis
+import Category, {categories, filterCategory} from "./componentes/Category";
 import Container from "./componentes/Container";
 import Footer from "./componentes/Footer";
 import Header from "./componentes/Header";
-import videos from "./json/videos.json";
-
-// exporta a const CATEGORIES de acordo com o arquivo json
-const categories = [...new Set(videos.map(video => video.categoria))];
-
-// filtrar videos de categoria X
-
-function filterCategory(id) {
-  return videos.filter(
-    video => video.categoria == categories[id]
-  );
-}
-
 
 function App() {
   return (
--- src/componentes/Category/index.js
+++ src/componentes/Category/index.js
@@ -1,5 +1,20 @@
 import styles from './Category.module.css';
+// import videos from "../ json/videos.json";
+import videos from '../../json/videos.json';
 
+/**
+ * Sem o EXPORT na constante e na funções os arquivos externos não conseguem exergá-lo
+ */
+
+// exporta a const CATEGORIES de acordo com o arquivo json
+export const categories = [...new Set(videos.map(video => video.categoria))];
+
+// filtrar videos de categoria X
+export function filterCategory(id) {
+  return videos.filter(
+    video => video.categoria == categories[id]
+  );
+}
 
 function Category({ category, children }) {
   return (
